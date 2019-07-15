2a396087d00cb4badb1ad49a8f3750d247955325
David Lamm <david-lamm@web.de>
2019-07-15 18:23:01 +0200

    get_database works
    The get_database handler now returns a database with
    the given id, and returns 404 ifid was not found.

--- src/handlers/databases.rs
+++ src/handlers/databases.rs
@@ -8,6 +8,7 @@ use crate::schema;
 use crate::models;
 use diesel::prelude::*;
 use log::debug;
+use crate::models::Database;
 
 #[get("/databases")]
 pub fn get_databases(req: HttpRequest) -> Box<Future<Item = HttpResponse, Error = Error>> {
@@ -20,12 +21,11 @@ pub fn get_databases(req: HttpRequest) -> Box<Future<Item = HttpResponse, Error 
         },
     };
 
-
-    let query = schema::databases::table.load::<models::Database>(&*conn);
     let query = schema::databases::table.inner_join(schema::access::table.on(schema::databases::columns::id.eq(schema::access::columns::object_id)))
         .filter(schema::access::columns::user_id.eq("549b60cd-9b88-467b-9b1e-b15c68114c96"))
         .select((schema::databases::columns::id, schema::databases::columns::name, schema::databases::columns::content))
         .load::<models::Database>(&*conn);
+
     match query {
         Ok(result) => {
             Box::new(Ok(HttpResponse::Ok().json(result)).into_future())
@@ -41,7 +41,28 @@ pub fn create_database(req: HttpRequest) -> Box<Future<Item = HttpResponse, Erro
 }
 #[get("/databases/{id}")]
 pub fn get_database(req: HttpRequest, id: web::Path<Uuid>) -> Box<Future<Item = HttpResponse, Error = Error>> {
-    Box::new(Ok(HttpResponse::NotImplemented().finish()).into_future())
+    let appdata: &AppData = req.app_data().unwrap();
+
+    let conn = match appdata.get_db_connection(){
+        Ok(connection) => connection,
+        Err(_) => {
+            return Box::new(Ok(HttpResponse::InternalServerError().finish()).into_future());
+        },
+    };
+
+    let query = schema::databases::table.find(format!("{}", id)).get_result::<Database>(&*conn);
+
+    match query {
+        Ok(result) => {
+            Box::new(Ok(HttpResponse::Ok().json(result)).into_future())
+        },
+        Err(e) => {
+            match e {
+                diesel::result::Error::NotFound => Box::new(Ok(HttpResponse::NotFound().finish()).into_future()),
+                _ => Box::new(Ok(HttpResponse::InternalServerError().finish()).into_future()),
+            }
+        }
+    }
 }
 #[put("/databases/{id}")]
 pub fn update_database(req: HttpRequest, id: web::Path<Uuid>) -> Box<Future<Item = HttpResponse, Error = Error>> {
