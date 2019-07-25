17b14d8e73a46c5ea6a5aec49f6db2478a9854a4
David Lamm <david-lamm@web.de>
2019-07-25 11:34:52 +0200

    more course handlers and AppData user getter/setter

--- src/handlers/courses.rs
+++ src/handlers/courses.rs
@@ -19,7 +19,41 @@ pub fn get_scope() -> Scope {
 
 #[get("")]
 fn get_courses(req: HttpRequest) -> Box<Future<Item = HttpResponse, Error = Error>> {
-    Box::new(Ok(HttpResponse::NotImplemented().finish()).into_future())
+    let appdata: &AppData = req.app_data().unwrap();
+
+    let conn = match appdata.get_db_connection(){
+        Ok(connection) => connection,
+        Err(_) => {
+            return Box::new(Ok(HttpResponse::InternalServerError().finish()).into_future());
+        },
+    };
+
+    let query = schema::courses::table.inner_join(schema::access::table.on(schema::courses::columns::id.eq(schema::access::columns::object_id)))
+        .filter(schema::access::columns::user_id.eq(appdata.current_user.to_string()))
+        .select((schema::courses::columns::id, schema::courses::columns::name, schema::courses::columns::description))
+        .load::<models::QueryableCourse>(&*conn);
+
+    match query {
+        Ok(query_courses) => {
+            let mut courses: Vec<models::Course> = Vec::new();
+            for course in query_courses {
+                let worksheets_query = schema::worksheets_in_courses::table
+                    .filter(schema::worksheets_in_courses::columns::course_id.eq(&course.id))
+                    .select((schema::worksheets_in_courses::columns::worksheet_id))
+                    .load::<String>(&*conn);
+                courses.push(models::Course {
+                    id: course.id,
+                    name: course.name,
+                    description: course.description,
+                    worksheets: worksheets_query.ok(),
+                });
+            }
+            Box::new(Ok(HttpResponse::Ok().json(courses)).into_future())
+        },
+        Err(e) => {
+            Box::new(Ok(HttpResponse::InternalServerError().finish()).into_future())
+        }
+    }
 }
 #[post("")]
 fn create_course(req: HttpRequest, json: web::Json<models::Course>) -> Box<Future<Item = HttpResponse, Error = Error>> {
@@ -66,5 +100,23 @@ fn update_course(req: HttpRequest, id: web::Path<Uuid>, json: web::Json<models::
 }
 #[delete("/{id}")]
 fn delete_course(req: HttpRequest, id: web::Path<Uuid>) -> Box<Future<Item = HttpResponse, Error = Error>> {
-    Box::new(Ok(HttpResponse::NotImplemented().finish()).into_future())
+    let appdata: &AppData = req.app_data().unwrap();
+
+    let conn = match appdata.get_db_connection(){
+        Ok(connection) => connection,
+        Err(_) => {
+            return Box::new(Ok(HttpResponse::InternalServerError().finish()).into_future());
+        },
+    };
+
+    let query = diesel::delete(schema::courses::table.find(format!("{}", id.into_inner())))
+        .execute(&*conn);
+    match query {
+        Ok(result) => {
+            Box::new(Ok(HttpResponse::Ok().finish()).into_future())
+        },
+        Err(e) => {
+            Box::new(Ok(HttpResponse::InternalServerError().finish()).into_future())
+        }
+    }
 }
