88c2fe8f09efd089be88a2f9fe0b218275f5157f
David Lamm <david-lamm@web.de>
2019-08-28 17:38:29 +0200

    migrations only support postgres

--- migrations/2019-08-28-134622_task-db-id-null/down.sql
+++ migrations/2019-08-28-134622_task-db-id-null/down.sql
@@ -1,10 +1,3 @@
 -- This file should undo anything in `up.sql`
-CREATE TABLE new_tasks (
-   id CHAR(36) PRIMARY KEY NOT NULL,
-   name TEXT,
-   database_id CHAR(36) NOT NULL,
-   FOREIGN KEY (database_id) REFERENCES databases(id)
-);
-INSERT INTO new_tasks SELECT tasks.id, tasks.name, tasks.database_id FROM tasks;
-DROP TABLE tasks;
-ALTER TABLE new_tasks RENAME TO tasks;
+DELETE FROM tasks WHERE database_id IS NULL;
+ALTER TABLE tasks ALTER COLUMN database_id SET NOT NULL;
