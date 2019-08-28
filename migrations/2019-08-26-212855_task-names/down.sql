d662e4cd300819aab729f3023ba2673b32b1b7cb
David Lamm <david-lamm@web.de>
2019-08-28 17:38:29 +0200

    migrations only support postgres

--- migrations/2019-08-26-212855_task-names/down.sql
+++ migrations/2019-08-26-212855_task-names/down.sql
@@ -1,9 +1,2 @@
 -- This file should undo anything in `up.sql`
-CREATE TABLE old_tasks (
-    id CHAR(36) PRIMARY KEY NOT NULL,
-    database_id CHAR(36) NOT NULL,
-    FOREIGN KEY (database_id) REFERENCES databases(id)
-);
-INSERT INTO old_tasks SELECT tasks.id, tasks.database_id FROM tasks;
-DROP TABLE tasks;
-ALTER TABLE old_tasks RENAME TO tasks;
+ALTER TABLE tasks DROP COLUMN IF EXISTS name;
--- migrations/2019-08-28-134622_task-db-id-null/up.sql
+++ migrations/2019-08-28-134622_task-db-id-null/up.sql
@@ -1,10 +1,2 @@
 -- Your SQL goes here
-CREATE TABLE new_tasks (
-   id CHAR(36) PRIMARY KEY NOT NULL,
-   database_id CHAR(36),
-   name TEXT,
-   FOREIGN KEY (database_id) REFERENCES databases(id)
-);
-INSERT INTO new_tasks (id, database_id, name) SELECT id, database_id, name FROM tasks;
-DROP TABLE tasks;
-ALTER TABLE new_tasks RENAME TO tasks;
+ALTER TABLE tasks ALTER COLUMN database_id DROP NOT NULL;
