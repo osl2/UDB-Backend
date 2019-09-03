0fd127e92e48f18410c929b254ca248305aae095
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2019-09-04 00:59:29 +0200

    does this fucking work now?!

--- migrations/2019-09-03-220841_database_name_not_unique/down.sql
+++ migrations/2019-09-03-220841_database_name_not_unique/down.sql
@@ -1,3 +1,2 @@
 -- This file should undo anything in `up.sql`
-CREATE UNIQUE INDEX databases_name_key ON public.databases USING btree (name);
-ALTER TABLE databases ADD CONSTRAINT databases_name_key UNIQUE USING INDEX databases_name_key;
+ALTER TABLE databases ADD CONSTRAINT databases_name_key UNIQUE (name);
